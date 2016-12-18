b473b44f07b6da2030a573e5c2d78d01d905667c
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2016-12-18 23:17:36 +0100

    create first marker

--- js/script.js
+++ js/script.js
@@ -48,4 +48,56 @@ function initMap() {
     center: {lat: 48.866667, lng: 2.333333},
     zoom: 12
   });
+
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 48.866667, lng: 2.333333},
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  
+    }); 
+
+// To add the marker to the map, call setMap();
+marker.setMap(map);
 }
+
+//VELIB
+
+// API KEY= 1ee25283f155079a4b54ddab39eac6d733b1fa49
+var velibJSON;
+
+$.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=1ee25283f155079a4b54ddab39eac6d733b1fa49", function( json ) {
+    velibJSON = json;
+    $.each(velibJSON, function(){
+        console.log(this.name);
+    })
+});
+
+
