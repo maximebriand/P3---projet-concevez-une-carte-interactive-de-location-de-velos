c127b4a71b29d5912524e60bcf31e460d45fb10b
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2016-12-24 11:05:37 +0100

    retrieve correct data in infoContent

--- js/script.js
+++ js/script.js
@@ -33,26 +33,45 @@ var selectedStationID, data, i, marker, velibJson;
                 (function(marker, json) {
 
                     // Attaching a click event to the current marker
-                    google.maps.event.addListener(marker, "click", function(e) {
-                        // infoWindow.setContent(description);
-                        // infoWindow.open(map, marker);
+ 					google.maps.event.addListener(marker, "click", function(e) {
+						
+						var address, places, availableBikes, availableBikeStands, banking, bonus, name;
+						address = json.address;
+						places = json.bike_stands;
+						availableBikes = json.available_bikes;
+						availableBikeStands = json.available_bike_stands;
+						banking = json.banking;
+						bonus = json.bonus;
+						name = json.name;
+						status = json.status;
+
                         $('#booking').show();
                         $('aside div.content').empty();
-                        $('aside div.content').append(
-                            "<h3 class=\"available_bikes\">Station : <span>" + json.name + "</span></h3> <ul>" +
-                            "<li class=\"available_bikes\">La station est <span>" + json.status + "</span></li>" +
-                            "<li class=\"available_bikes\">Adresse : <span>" + json.address + "</span></li>" +
-                            "<li class=\"available_bikes\"><span>" + json.bike_stands + " " + "placesDescription" +
-                            "<li class=\"available_bikes\"><span>" + json.available_bikes + " " + "availableBikesDescription" +
-                            "<li class=\"available_bikes\"><span>" + json.available_bike_stands + " " + "availableBikeStandsDescription" +
-                            "<li class=\"available_bikes\">Le paiement à cette station est <span>" + json.banking + "</span></li></ul>"
-                        );
                         selectedStationID = this.id;
+
+                        $('aside div.content').empty();
+                        var placesDescription, availableBikesDescription, availableBikeStandsDescription;
+                        
+                        if (places <= 1) { placesDescription = "</span> place à cette station</li>" } else { placesDescription = "</span> places à cette station</li>" };
+
+                        if (availableBikes <= 1) { availableBikesDescription = "</span> vélo de disponible</li>" } else { availableBikesDescription = "</span> vélos sont disponibles</li>" };
+
+                        if (availableBikeStands <= 1) { availableBikeStandsDescription = "</span> emplacement de libre</li>" } else { availableBikeStandsDescription = "</span> emplacements sont libres</li>" }
+
+                        $('aside div.content').append(
+                            "<h3 class=\"available_bikes\">Station : <span>" + name + "</span></h3> <ul>" +
+                            "<li class=\"available_bikes\">La station est <span>" + status + "</span></li>" +
+                            "<li class=\"available_bikes\">Adresse : <span>" + address + "</span></li>" +
+                            "<li class=\"available_bikes\"><span>" + places + " " + placesDescription +
+                            "<li class=\"available_bikes\"><span>" + availableBikes + " " + availableBikesDescription +
+                            "<li class=\"available_bikes\"><span>" + availableBikeStands + " " + availableBikeStandsDescription +
+                            "<li class=\"available_bikes\">Le paiement à cette station est <span>" + banking + "</span></li></ul>"
+                        );
                     });
 
                 })(marker, data);
             };
-            $('#booking').click(function(){
+            $('#booking').click(function() {
 
                 $('#canvas').show();
                 $('#sign').show();
