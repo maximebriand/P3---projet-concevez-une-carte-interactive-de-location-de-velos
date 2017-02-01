e7ec5981f3ac7603b31f0c738ff26f0f63575cfb
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-02-01 14:13:00 +0100

    rebuilt code to have objects and console log on click with data

--- js/script.js
+++ js/script.js
@@ -1,51 +1,44 @@
-var markers = [];
+var googleMap = {
+    apiUrl: null,
+    map: null,
 
-function initMap() {
-    var parisCenter = new google.maps.LatLng(48.866667, 2.333333);
-    var myOptions = {
-        zoom: 15,
-        center: parisCenter,
-        scrollwheel: false,
-    }
-    map = new google.maps.Map(document.getElementById("map"), myOptions);
-};
-
-//add a marker to the map.
-function createMarker(location) {
-    marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-};
-
-
-// for each avec toutes les coordonnées dans un objets
-function addMarkers() {
-    $.each(markers, function(){
-        NewMarker = new google.maps.LatLng(this.latitude, this.longitude);
-        createMarker(NewMarker);
-        google.maps.event.addListener(marker, 'click', function() {
-            console.log(this.name)
+    init: function(mapElt, centerLat, centerLng, zoom ) {
+        this.map = new google.maps.Map(mapElt, {
+            center: new google.maps.LatLng(centerLat, centerLng),
+            zoom: zoom,
+            
         });
-    });
-};
-
-
-var JCDecaux = $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=1ee25283f155079a4b54ddab39eac6d733b1fa49")
-    .done(function(json) {
-        for (var i = 0, length = json.length; i < length; i++) {
-            var data = json[i];        
-            markers.push({
-                longitude: data.position.lng,
-                latitude: data.position.lat,
-                status: data.status,
-                name: data.name
+        return map
+    },
+    addMarker: function(url) {
+        $.getJSON(url, function(json) {
+            $.each(json, function(index, value){
+                googleMap.createMarker(json[index]);
             });
-        };
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() { //always() reçoit en paramètre une fonction de rappel qui permet de préciser le comportement du programme après l'appel AJAX, que l'appel ait été réussi ou non.
-        addMarkers();
-    });
+        });
+    },
+
+    createMarker: function(stationsInfo) {
+        console.log(stationsInfo.position.lat);
+        NewMarker = new google.maps.LatLng(stationsInfo.position.lat, stationsInfo.position.lng);
+        googleMap.displayMarker(NewMarker);
+        google.maps.event.addListener(marker, 'click', function() {
+            console.log(stationsInfo.name)
+        });
+    },
+
+    displayMarker: function(location) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: this.map,
+        })
+    }
+
+}
+
+ 
+$(function(){
+    var apiUrl = "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=1ee25283f155079a4b54ddab39eac6d733b1fa49";
+    googleMap.init(document.getElementById("map"), 48.866667, 2.333333, 15);
+    googleMap.addMarker(apiUrl);
+})
